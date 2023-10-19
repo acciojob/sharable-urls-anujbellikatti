2a7626a310d6c259e06bae8975a06a3bf98bcdfd
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-19 23:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+let btn = document.getElementById("button");
+let url = document.getElementById("url");
+
+
+
+// btn.addEventListner("click",f());
+
+function f(){
+	let name1=name.value;
+	let year1 = year.value;
+	if(name1!==""&&year1!==""){
+	url.innerHTML="https://localhost:8080/?name="+name1+"&year="+year1;
+	}
+	else if(name1===""&&year1!==""){
+	url.innerHTML="https://localhost:8080/?year="+year1;
+	}else if(name1!==""&&year1===""){
+	url.innerHTML="https://localhost:8080/?name="+name1;
+	}
+	
+}
